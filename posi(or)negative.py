690ac925fc242135ee5b9ebe45b3f1b967f33141
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-04 22:57:07 +0530

    Add files via upload

---
+++ posi(or)negative.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number:"))
+if(a%2 == 0):
+    print("even")
+else:
+    print("Odd")  
+if(a>0):
+    print("Positive")
+elif(a<0):
+    print("Negative")
+else:
+    print("zero")
+       
