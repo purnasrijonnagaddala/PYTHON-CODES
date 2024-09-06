5cf31cbca1546c007e00051d72bcd6b24dcbf3db
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-06 22:59:02 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,12 @@
+
+
+n1=int(input())
+n2=int(input())
+count=0
+for i in range(n1,n2+1):
+    if i>1:
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=" ")
