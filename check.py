c008886b5cbd1ad1b8e2bf465f4bda72fc583e5f
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-08 23:07:44 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes two lists as input and checks whether they are equal (i.e., have the same elements in the same order).
+a = ["apple","kiwi","papaya"]
+b = ["apple","kiwi","papaya"]
+if(a==b):
+    print("Both lists are equal")
