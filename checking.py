8c2105abdf4c259d0271107345490edffd111073
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-08 23:09:07 +0530

    Add files via upload

---
+++ checking.py
@@ -0,0 +1,7 @@
+#Write a Python program that takes a list of elements and a target element as input, and checks whether the target element is present in the list using the in keyword or the __contains__() method.
+a = [4,9,6,1,8]
+b = 9
+#if a.__contains__(b):
+if(b in a):
+    print("The element is present")
+
