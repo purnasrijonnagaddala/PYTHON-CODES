03ee140c23839bda3f31afc3e2abbd85f7a25da1
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-08 23:10:07 +0530

    Add files via upload

---
+++ combining.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes two lists as input and combines them into a single list using the extend() method or the + operator
+a = ["python","java","c"]
+b = [4,7,1,9]
+c = a+b
+print(c)
