608887dd83c60e22b718ef3c346d5b49f6e89630
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-10 22:44:48 +0530

    Add files via upload

---
+++ findindex.py
@@ -0,0 +1,6 @@
+#Write a Python program that takes a list of elements and a target element as input, and returns the index of the first occurrence of the target element using the index() method.
+a = ["apple","banana","kiwi","papaya"]
+b = a.index("kiwi")
+print(b)
+
+#2
