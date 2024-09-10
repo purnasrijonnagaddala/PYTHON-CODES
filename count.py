d16204f2a23fc02df668a3c3ba87c1fa10ce9793
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-10 22:44:26 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes a list of elements as input and counts the occurrences of a specific element using the count() method.
+a = ["apple","kiwi","papaya","apple"]
+print(a.count("apple"))
+
+#2
