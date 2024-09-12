05e240761004d6c896e50b6438678283b4476135
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-12 15:12:34 +0530

    Add files via upload

---
+++ reversel.py
@@ -0,0 +1,6 @@
+#Write a Python program that takes a list as input and reverses the order of its elements using the reverse() method.
+a = ["apple","kiwi","cherry","papaya"]
+a.reverse()
+print(a)
+
+#['papaya', 'cherry', 'kiwi', 'apple']
