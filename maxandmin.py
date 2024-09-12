cb8ff2b9632b1e3b235644c34e84a48bad83ab5e
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-12 15:11:24 +0530

    Add files via upload

---
+++ maxandmin.py
@@ -0,0 +1,5 @@
+#Write a Python program that takes a list of numbers as input and finds the maximum and minimum values using the max() and min() functions.
+a = [5,3,9,7,1,4]
+print(min(a))
+print(max(a))
+
