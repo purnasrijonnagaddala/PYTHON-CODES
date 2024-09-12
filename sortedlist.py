68efe6e7fa5929daea19c13e3fc6f6c393747695
purnasrijonnagaddala <104913519+purnasrijonnagaddala@users.noreply.github.com>
2024-09-12 15:13:43 +0530

    Add files via upload

---
+++ sortedlist.py
@@ -0,0 +1,7 @@
+#Write a Python program that takes a list of numbers as input and sorts them in ascending and descending order using the sort() method with the reverse parameter.
+a = [8,4,7,2,5]
+a.sort()
+print(a)
+a.reverse()
+print(a)
+
